ff3473af24a4e68401a6c1482040961a3383aab3
6ot9lpa <sp1vaks@yandex.ru>
2026-07-30 03:22:06 +0300

    perf: tune Tiny2 continued training config

--- tests/training/test_rubert_training_config.py
+++ tests/training/test_rubert_training_config.py
@@ -24,3 +24,18 @@ def test_distil_conversational_config_uses_memory_safe_effective_batch() -> None
         * config.training.gradient_accumulation_steps
         == 64
     )
+
+
+def test_tiny2_continued_config_uses_benchmarked_gpu_limits() -> None:
+    config = RuBertTrainingConfig.load(
+        "configs/training/rubert_tiny2_continued.yaml"
+    )
+
+    assert config.model.base_model_name == "cointegrated/rubert-tiny2"
+    assert config.model.classifier_output_dir.name.endswith("trained-20260729")
+    assert config.training.train_batch_size == 48
+    assert config.training.eval_batch_size == 128
+    assert config.training.gradient_accumulation_steps == 1
+    assert config.training.learning_rate == 5e-6
+    assert config.training.num_train_epochs == 2
+    assert config.training.gradient_checkpointing is False
